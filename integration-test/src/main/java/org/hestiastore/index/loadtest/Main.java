bdbffd225b27ae0a52e811dfde2d8d93b9054dc0
Jan Jirout <jirout@gmail.com>
2025-05-27 20:11:36 +0200

    release: version 0.0.3

--- integration-test/src/main/java/org/hestiastore/index/loadtest/Main.java
+++
@@ -1,9 +0,0 @@
-package org.hestiastore.index.loadtest;
-
-public class Main {
-
-    public static void main(final String[] args) throws Exception {
-        new LoadTestCli(args);
-    }
-
-}
--- jmh-benchmarks/src/main/java/org/hestiastore/index/benchmark/MainBenchmarkClass.java
+++
@@ -1,8 +0,0 @@
-package org.hestiastore.index.benchmark;
-
-public class MainBenchmarkClass {
-
-    public static void main(String[] args) throws Exception {
-        org.openjdk.jmh.Main.main(args);
-    }
-}
