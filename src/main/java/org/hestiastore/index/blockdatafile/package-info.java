87660d29a31edfdfc04cab02cb4f756f67640865
Jan Jirout <jirout@gmail.com>
2025-07-18 18:00:44 +0200

    starting on lightweight pages - blocks #31

---
+++ src/main/java/org/hestiastore/index/blockdatafile/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * packe for writing and reading sorted data from file splitted into possible
+ * cached bock. Blocks have same size.
+ * 
+ */
+package org.hestiastore.index.blockdatafile;
