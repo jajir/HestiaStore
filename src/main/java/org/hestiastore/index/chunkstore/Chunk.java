18bcfbbc6d088b4ba6388d7db4f4c3d722cfbf7e
Jan Jirout <jirout@gmail.com>
2025-08-03 22:03:34 +0200

    Chunk data structure was extended :-)

--- src/main/java/org/hestiastore/index/chunkstore/Chunk.java
+++ src/main/java/org/hestiastore/index/chunkstore/Chunk.java
@@ -4,7 +4,24 @@ public class Chunk {
 
     static final int HEADER_SIZE = 32;
 
-    // "theodora" in ASCII
+    /**
+     * Size of cell in bytes. Cell is smalles addresable unit in chunk store.
+     */
+    static final int CELL_SIZE = 16;
+
+    /**
+     * "theodora" in ASCII
+     */
     static final long MAGIC_NUMBER = 0x7468656F646F7261L;
+
+    /**
+     * Version of the chunk format. It is used to identify the format of the
+     * chunk and ensure compatibility between different versions of the chunk
+     * format.
+     * 
+     * real version is remotely related to main library version when it's
+     * introduced.
+     */
+    static final int VERSION = 0xff_00_00_05;
 
 }
---
+++ src/test/java/org/hestiastore/index/chunkstore/ChunkHeaderTest.java
@@ -0,0 +1,40 @@
+package org.hestiastore.index.chunkstore;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class ChunkHeaderTest {
+
+    private final static int VERSION = 1000000;
+    private final static int PAYLOAD_LENGTH = 12345;
+    private final static long CRC = 67890L;
+
+    @Test
+    void test_store_and_load() {
+        final ChunkHeader header1 = ChunkHeader.of(Chunk.MAGIC_NUMBER, VERSION,
+                PAYLOAD_LENGTH, CRC);
+
+        final byte[] data = header1.toBytes().getData();
+        final ChunkHeader header2 = ChunkHeader.of(data);
+
+        assertEquals(Chunk.MAGIC_NUMBER, header2.getMagicNumber());
+        assertEquals(VERSION, header2.getVersion());
+        assertEquals(PAYLOAD_LENGTH, header2.getPayloadLength());
+        assertEquals(CRC, header2.getCrc());
+    }
+
+    @Test
+    void test_of_invalid_byte_length() {
+        final byte[] data = new byte[Chunk.HEADER_SIZE + 1];
+
+        final Exception e = assertThrows(IllegalArgumentException.class,
+                () -> ChunkHeader.of(data));
+
+        assertEquals("Invalid chunk header size '33', expected is '32'",
+                e.getMessage());
+
+    }
+
+}
