53742ae90f438b495528683a5223a05d6f115988
Jan Jirout <jirout@gmail.com>
2025-09-27 09:27:20 +0200

    Update BlockWriter.java

--- src/main/java/org/hestiastore/index/datablockfile/BlockWriter.java
+++ src/main/java/org/hestiastore/index/datablockfile/BlockWriter.java
@@ -3,7 +3,10 @@ package org.hestiastore.index.datablockfile;
 import org.hestiastore.index.Bytes;
 
 public interface BlockWriter extends AutoCloseable {
-
+//TODO replace AutoCloseable with CloasebleResource
+//TODO it this class used?
+//TODO javadoc
+    
     /**
      * Writes a DataBlock to the block file.
      *
