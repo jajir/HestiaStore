2abf2747a7ddf00447ccf96ff61066b048266b87
Jan Jirout <jirout@gmail.com>
2025-10-20 18:47:42 +0200

    unsorted data file was improved

--- src/main/java/org/hestiastore/index/unsorteddatafile/UnsortedDataFile.java
+++ src/main/java/org/hestiastore/index/unsorteddatafile/UnsortedDataFile.java
@@ -2,90 +2,23 @@ package org.hestiastore.index.unsorteddatafile;
 
 import org.hestiastore.index.PairIterator;
 import org.hestiastore.index.PairIteratorStreamer;
-import org.hestiastore.index.Vldtn;
-import org.hestiastore.index.datatype.TypeReader;
-import org.hestiastore.index.datatype.TypeWriter;
-import org.hestiastore.index.directory.Directory;
-import org.hestiastore.index.directory.FileReader;
 
 /**
- * Unsorted key value pairs storage file.
- * 
- * @author honza
+ * Abstraction for unsorted key-value storage backed by the {@code Directory}
+ * API.
  *
  * @param <K> key type
  * @param <V> value type
  */
-public class UnsortedDataFile<K, V> {
+public interface UnsortedDataFile<K, V> {
 
-    private final Directory directory;
-
-    private final String fileName;
-
-    private final TypeWriter<K> keyWriter;
-
-    private final TypeWriter<V> valueWriter;
-
-    private final TypeReader<K> keyReader;
-
-    private final TypeReader<V> valueReader;
-
-    private final int diskIoBufferSize;
-
-    public static <M, N> UnsortedDataFileBuilder<M, N> builder() {
-        return new UnsortedDataFileBuilder<M, N>();
+    static <K, V> UnsortedDataFileBuilder<K, V> builder() {
+        return new UnsortedDataFileBuilder<>();
     }
 
-    public UnsortedDataFile(final Directory directory, final String fileName,
-            final TypeWriter<K> keyWriter, final TypeWriter<V> valueWriter,
-            final TypeReader<K> keyReader, final TypeReader<V> valueReader,
-            final int diskIoBufferSize) {
-        this.directory = Vldtn.requireNonNull(directory, "directory");
-        this.fileName = Vldtn.requireNonNull(fileName, "fileName");
-        this.keyWriter = Vldtn.requireNonNull(keyWriter, "keyWriter");
-        this.valueWriter = Vldtn.requireNonNull(valueWriter, "");
-        this.keyReader = Vldtn.requireNonNull(keyReader, "");
-        this.valueReader = Vldtn.requireNonNull(valueReader, "valueReader");
-        this.diskIoBufferSize = diskIoBufferSize;
-    }
+    PairIterator<K, V> openIterator();
 
-    /**
-     * Opens an iterator for this file. If the file does not exist, an empty
-     * iterator is returned.
-     * 
-     * @return opened iterator
-     */
-    public PairIterator<K, V> openIterator() {
-        return new DataFileIterator<>(keyReader, valueReader, getFileReader());
-    }
+    PairIteratorStreamer<K, V> openStreamer();
 
-    /**
-     * Opens a transaction writer for this file. If the file does not exist, it
-     * is created.
-     * 
-     * @return a UnsortedDataFileWriterTx instance
-     */
-    public UnsortedDataFileWriterTx<K, V> openWriterTx() {
-        return new UnsortedDataFileWriterTx<>(fileName, directory,
-                diskIoBufferSize, keyWriter, valueWriter);
-    }
-
-    /**
-     * Opens a streamer for this file. If the file does not exist, an empty
-     * streamer is returned.
-     * 
-     * @return opened streamer
-     */
-    public PairIteratorStreamer<K, V> openStreamer() {
-        if (directory.isFileExists(fileName)) {
-            return new PairIteratorStreamer<>(openIterator());
-        } else {
-            return new PairIteratorStreamer<>(null);
-        }
-    }
-
-    private FileReader getFileReader() {
-        return directory.getFileReader(fileName, diskIoBufferSize);
-    }
-
+    UnsortedDataFileWriterTx<K, V> openWriterTx();
 }
