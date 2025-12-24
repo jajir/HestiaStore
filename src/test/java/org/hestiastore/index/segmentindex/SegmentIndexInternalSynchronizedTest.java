818b26c3c1ac57da0ae417d86391f093e0276bdf
Jan Jirout <jirout@gmail.com>
2025-12-24 17:39:14 +0100

    Refactor SegmentIndexInternalSynchronizedTest to use IndexConfiguration builder; enhance constructor test for better clarity and maintainability

--- src/test/java/org/hestiastore/index/segmentindex/SegmentIndexInternalSynchronizedTest.java
+++ src/test/java/org/hestiastore/index/segmentindex/SegmentIndexInternalSynchronizedTest.java
@@ -1,40 +1,55 @@
 package org.hestiastore.index.segmentindex;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import java.util.List;
+
+import org.hestiastore.index.chunkstore.ChunkFilterDoNothing;
 import org.hestiastore.index.datatype.TypeDescriptor;
 import org.hestiastore.index.datatype.TypeDescriptorInteger;
 import org.hestiastore.index.datatype.TypeDescriptorShortString;
 import org.hestiastore.index.directory.Directory;
 import org.hestiastore.index.directory.MemDirectory;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 class SegmentIndexInternalSynchronizedTest {
 
     private static final TypeDescriptor<Integer> TD_INTEGER = new TypeDescriptorInteger();
     private static final TypeDescriptor<String> TD_STRING = new TypeDescriptorShortString();
 
-    private Directory directory = new MemDirectory();
-
-    @Mock
-    private IndexConfiguration<Integer, String> conf;
+    private final Directory directory = new MemDirectory();
 
     @Test
     void test_constructor() {
-        when(conf.getMaxNumberOfSegmentsInCache()).thenReturn(1000);
-        when(conf.getNumberOfThreads()).thenReturn(1);
-        try (IndexInternalSynchronized<Integer, String> synchIndex = new IndexInternalSynchronized<>(
-                directory, TD_INTEGER, TD_STRING, conf)) {
-            // Intentionally left empty to test constructor
-        }
-
-        // If no exception is thrown, then constructor works as expected
-        assertTrue(true);
+        final IndexConfiguration<Integer, String> conf = IndexConfiguration
+                .<Integer, String>builder()//
+                .withKeyClass(Integer.class)//
+                .withValueClass(String.class)//
+                .withKeyTypeDescriptor(TD_INTEGER)//
+                .withValueTypeDescriptor(TD_STRING)//
+                .withName("test-index")//
+                .withMaxNumberOfKeysInSegmentCache(4)//
+                .withMaxNumberOfKeysInSegmentCacheDuringFlushing(6)//
+                .withMaxNumberOfKeysInSegmentChunk(2)//
+                .withMaxNumberOfKeysInCache(10)//
+                .withMaxNumberOfKeysInSegment(20)//
+                .withMaxNumberOfSegmentsInCache(3)//
+                .withBloomFilterNumberOfHashFunctions(1)//
+                .withBloomFilterIndexSizeInBytes(1024)//
+                .withBloomFilterProbabilityOfFalsePositive(0.01D)//
+                .withDiskIoBufferSizeInBytes(1024)//
+                .withContextLoggingEnabled(false)//
+                .withEncodingFilters(List.of(new ChunkFilterDoNothing()))//
+                .withDecodingFilters(List.of(new ChunkFilterDoNothing()))//
+                .withNumberOfCpuThreads(1)//
+                .withNumberOfIoThreads(1)//
+                .build();
+        assertDoesNotThrow(() -> {
+            try (IndexInternalSynchronized<Integer, String> synchIndex = new IndexInternalSynchronized<>(
+                    directory, TD_INTEGER, TD_STRING, conf)) {
+                // constructor exercised
+            }
+        });
     }
 
 }
