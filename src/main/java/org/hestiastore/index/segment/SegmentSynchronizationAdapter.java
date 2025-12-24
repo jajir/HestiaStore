d2825e1c334e6bf47d481cc9d6b519b8ef638aef
Jan Jirout <jirout@gmail.com>
2025-12-24 22:39:05 +0100

    Refactor EntryIteratorWithLock and SegmentSynchronizationAdapter to use ReentrantReadWriteLock; enhance concurrency handling and improve iterator behavior during concurrent writes

--- src/main/java/org/hestiastore/index/segment/SegmentSynchronizationAdapter.java
+++ src/main/java/org/hestiastore/index/segment/SegmentSynchronizationAdapter.java
@@ -1,6 +1,7 @@
 package org.hestiastore.index.segment;
 
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.hestiastore.index.AbstractCloseableResource;
 import org.hestiastore.index.Entry;
@@ -9,14 +10,16 @@ import org.hestiastore.index.EntryWriter;
 
 /**
  * Serializes access to a single {@link Segment} instance using a dedicated
- * {@link ReentrantLock}. Iterators and writers are wrapped so that their
- * operations synchronize on the same lock.
+ * {@link ReentrantReadWriteLock}. Read operations can run concurrently, while
+ * writers/compaction are exclusive and block readers for their duration.
  */
 public class SegmentSynchronizationAdapter<K, V> extends AbstractCloseableResource
         implements Segment<K, V> {
 
     private final Segment<K, V> delegate;
-    private final ReentrantLock lock = new ReentrantLock();
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Lock readLock = lock.readLock();
+    private final Lock writeLock = lock.writeLock();
 
     public SegmentSynchronizationAdapter(final Segment<K, V> delegate) {
         this.delegate = delegate;
@@ -24,145 +27,147 @@ public class SegmentSynchronizationAdapter<K, V> extends AbstractCloseableResour
 
     @Override
     public SegmentStats getStats() {
-        lock.lock();
+        readLock.lock();
         try {
             return delegate.getStats();
         } finally {
-            lock.unlock();
+            readLock.unlock();
         }
     }
 
     @Override
     public long getNumberOfKeys() {
-        lock.lock();
+        readLock.lock();
         try {
             return delegate.getNumberOfKeys();
         } finally {
-            lock.unlock();
+            readLock.unlock();
         }
     }
 
     @Override
     public void optionallyCompact() {
-        lock.lock();
+        writeLock.lock();
         try {
             delegate.optionallyCompact();
         } finally {
-            lock.unlock();
+            writeLock.unlock();
         }
     }
 
     @Override
     public void forceCompact() {
-        lock.lock();
+        writeLock.lock();
         try {
             delegate.forceCompact();
         } finally {
-            lock.unlock();
+            writeLock.unlock();
         }
     }
 
     @Override
     public K checkAndRepairConsistency() {
-        lock.lock();
+        writeLock.lock();
         try {
             return delegate.checkAndRepairConsistency();
         } finally {
-            lock.unlock();
+            writeLock.unlock();
         }
     }
 
     @Override
     public EntryIterator<K, V> openIterator() {
-        lock.lock();
+        readLock.lock();
         try {
             final EntryIterator<K, V> iterator = delegate.openIterator();
-            return new LockedEntryIterator<>(iterator, lock);
-        } finally {
-            lock.unlock();
+            return new LockedEntryIterator<>(iterator, readLock);
+        } catch (final Throwable t) {
+            readLock.unlock();
+            throw t;
         }
     }
 
     @Override
     public EntryWriter<K, V> openDeltaCacheWriter() {
-        lock.lock();
+        writeLock.lock();
         try {
             final EntryWriter<K, V> writer = delegate.openDeltaCacheWriter();
-            return new LockedEntryWriter<>(writer, lock);
-        } finally {
-            lock.unlock();
+            return new LockedEntryWriter<>(writer, writeLock);
+        } catch (final Throwable t) {
+            writeLock.unlock();
+            throw t;
         }
     }
 
     @Override
     public V get(final K key) {
-        lock.lock();
+        readLock.lock();
         try {
             return delegate.get(key);
         } finally {
-            lock.unlock();
+            readLock.unlock();
         }
     }
 
     @Override
     public Segment<K, V> createSegmentWithSameConfig(
             final SegmentId segmentId) {
-        lock.lock();
+        readLock.lock();
         try {
             return new SegmentSynchronizationAdapter<>(
                     delegate.createSegmentWithSameConfig(segmentId));
         } finally {
-            lock.unlock();
+            readLock.unlock();
         }
     }
 
     @Override
     public SegmentSplitterPolicy<K, V> getSegmentSplitterPolicy() {
-        lock.lock();
+        readLock.lock();
         try {
             return delegate.getSegmentSplitterPolicy();
         } finally {
-            lock.unlock();
+            readLock.unlock();
         }
     }
 
     @Override
     public SegmentSplitter<K, V> getSegmentSplitter() {
-        lock.lock();
+        readLock.lock();
         try {
             return delegate.getSegmentSplitter();
         } finally {
-            lock.unlock();
+            readLock.unlock();
         }
     }
 
     @Override
     public SegmentId getId() {
-        lock.lock();
+        readLock.lock();
         try {
             return delegate.getId();
         } finally {
-            lock.unlock();
+            readLock.unlock();
         }
     }
 
     @Override
     public int getVersion() {
-        lock.lock();
+        readLock.lock();
         try {
             return delegate.getVersion();
         } finally {
-            lock.unlock();
+            readLock.unlock();
         }
     }
 
     @Override
     protected void doClose() {
-        lock.lock();
+        writeLock.lock();
         try {
             delegate.close();
         } finally {
-            lock.unlock();
+            writeLock.unlock();
         }
     }
 
@@ -170,37 +175,26 @@ public class SegmentSynchronizationAdapter<K, V> extends AbstractCloseableResour
             extends AbstractCloseableResource implements EntryIterator<K, V> {
 
         private final EntryIterator<K, V> delegate;
-        private final ReentrantLock lock;
+        private final Lock lock;
 
         LockedEntryIterator(final EntryIterator<K, V> delegate,
-                final ReentrantLock lock) {
+                final Lock lock) {
             this.delegate = delegate;
             this.lock = lock;
         }
 
         @Override
         public boolean hasNext() {
-            lock.lock();
-            try {
-                return delegate.hasNext();
-            } finally {
-                lock.unlock();
-            }
+            return delegate.hasNext();
         }
 
         @Override
         public Entry<K, V> next() {
-            lock.lock();
-            try {
-                return delegate.next();
-            } finally {
-                lock.unlock();
-            }
+            return delegate.next();
         }
 
         @Override
         protected void doClose() {
-            lock.lock();
             try {
                 delegate.close();
             } finally {
@@ -213,27 +207,21 @@ public class SegmentSynchronizationAdapter<K, V> extends AbstractCloseableResour
             extends AbstractCloseableResource implements EntryWriter<K, V> {
 
         private final EntryWriter<K, V> delegate;
-        private final ReentrantLock lock;
+        private final Lock lock;
 
         LockedEntryWriter(final EntryWriter<K, V> delegate,
-                final ReentrantLock lock) {
+                final Lock lock) {
             this.delegate = delegate;
             this.lock = lock;
         }
 
         @Override
         public void write(final Entry<K, V> entry) {
-            lock.lock();
-            try {
-                delegate.write(entry);
-            } finally {
-                lock.unlock();
-            }
+            delegate.write(entry);
         }
 
         @Override
         protected void doClose() {
-            lock.lock();
             try {
                 delegate.close();
             } finally {
