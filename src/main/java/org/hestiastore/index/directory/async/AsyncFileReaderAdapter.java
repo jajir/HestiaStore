7d9be254f49afcb5141feed5d1c5e2f555bd6017
Jan Jirout <jirout@gmail.com>
2025-12-29 20:02:18 +0100

    Remove unnecessary locking in AsyncFileReaderAdapter and AsyncFileWriterAdapter for improved performance

--- src/main/java/org/hestiastore/index/directory/async/AsyncFileReaderAdapter.java
+++ src/main/java/org/hestiastore/index/directory/async/AsyncFileReaderAdapter.java
@@ -4,7 +4,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.Executor;
 import java.util.function.Supplier;
-import java.util.concurrent.locks.ReentrantLock;
 
 import org.hestiastore.index.AbstractCloseableResource;
 import org.hestiastore.index.Vldtn;
@@ -19,8 +18,6 @@ class AsyncFileReaderAdapter extends AbstractCloseableResource
 
     private final FileReader delegate;
     private final Executor executor;
-    private final ReentrantLock lock = new ReentrantLock();
-
     AsyncFileReaderAdapter(final FileReader delegate, final Executor executor) {
         this.delegate = Vldtn.requireNonNull(delegate, "delegate");
         this.executor = Vldtn.requireNonNull(executor, "executor");
@@ -46,12 +43,7 @@ class AsyncFileReaderAdapter extends AbstractCloseableResource
 
     @Override
     protected void doClose() {
-        lock.lock();
-        try {
-            delegate.close();
-        } finally {
-            lock.unlock();
-        }
+        delegate.close();
     }
 
     protected <T> CompletionStage<T> supply(final Supplier<T> supplier) {
@@ -60,17 +52,11 @@ class AsyncFileReaderAdapter extends AbstractCloseableResource
                     "AsyncFileReader already closed"));
         }
         return CompletableFuture.supplyAsync(() -> {
-            lock.lock();
-            try {
-                if (wasClosed()) {
-                    throw new IllegalStateException(
-                            "AsyncFileReader already closed");
-                }
-                return supplier.get();
-            } finally {
-                lock.unlock();
+            if (wasClosed()) {
+                throw new IllegalStateException(
+                        "AsyncFileReader already closed");
             }
+            return supplier.get();
         }, executor);
     }
 }
-
