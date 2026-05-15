72487dac58173ae07738c6b5da7ed9d82cab1b88
“Jan <jirout@gmail.com>
2026-05-15 18:00:24 +0200

    Refactor segment access to segment lease
    - Replaced SegmentAccess and SegmentAccessService with SegmentLease and SegmentLeaseService in IndexOperationCoordinator and related classes.
    - Introduced SegmentLease and SegmentLeaseService interfaces to provide scoped blocking leases for segments.
    - Implemented SegmentLeaseImpl and SegmentLeaseServiceImpl to manage segment leases.
    - Updated tests to reflect changes from segment access to segment lease.
    - Removed SegmentAccessService as it is no longer needed.

--- engine/src/main/java/org/hestiastore/index/segmentindex/core/segmentlease/SegmentLease.java
+++ engine/src/main/java/org/hestiastore/index/segmentindex/core/segmentlease/SegmentLease.java
@@ -1,15 +1,15 @@
-package org.hestiastore.index.segmentindex.core.segmentaccess;
+package org.hestiastore.index.segmentindex.core.segmentlease;
 
 import org.hestiastore.index.segment.SegmentId;
 import org.hestiastore.index.segmentregistry.BlockingSegment;
 
 /**
- * Scoped access to the segment currently owning one routed key.
+ * Scoped lease for the segment currently owning one routed key.
  *
  * @param <K> key type
  * @param <V> value type
  */
-public interface SegmentAccess<K, V> extends AutoCloseable {
+public interface SegmentLease<K, V> extends AutoCloseable {
 
     /**
      * Returns the acquired segment id.
